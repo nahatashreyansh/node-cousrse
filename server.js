df69332e20a8488fa903034e12e80b9c0beb6835
shreyansh nahata <shreyanshnahata03@gmail.com>
2018-04-28 14:50:54 +0530

    server changes and package file

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const fs = require('fs');
+const port = process.env.PORT || 3000; // This port value comes from the prt value heroku has set up.
+
 var app = express();
 const hbs = require('hbs');
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public')); //middleware
 app.use((req, res, next) =>{
   var now = new Date().toString();
   var log = now + ',' + req.method + ',' + req.url ;
@@ -15,6 +16,8 @@ app.use((req, res, next) =>{
   });
   next();
 });
+app.use(express.static(__dirname + '/public')); //middleware
+
 hbs.registerHelper('getCurrentYear', () =>{
   return new Date().getFullYear();
 });
@@ -42,6 +45,6 @@ app.get('/about',(req,res)=>{
     pageTitle : 'About Page'
   });
 });
-app.listen(3000 , () => {
-  console.log('Server is up on port 3000');
+app.listen(port , () => {
+  console.log('Server is up on port' + port);
 });
